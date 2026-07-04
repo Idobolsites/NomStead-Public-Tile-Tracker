ccc0851cf349940f554d0c1ca1a7206107330807
Idobolsites <larswuensch93@gmail.com>
2026-07-04 21:02:18 +0200

    Add files via upload

---
+++ gf.js
@@ -0,0 +1,7 @@
+const gf = [
+
+{ id: "gold-furnace-1", type: "Gold Furnace", customLink: "https://nomstead.com/gardengully" },
+];
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = gf;
+}
